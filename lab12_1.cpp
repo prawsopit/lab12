135ca788d1aa8c2bb39ccaf21ab5b6569abb9669
prawsopit <prawsopit@gmail.com>
2023-01-06 15:47:34 +0700

    12.1

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,30 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+using namespace std ;
+
+int main()
+{
+cout << "Press Enter 3 times to reveal your future.";
+for (int y=0; y<=2; y++){
+cin.get() ;
+
+}
+srand(time(0));
+int x = rand()%9;
+string z;
+if(x ==0) z ="A" ;
+else if (x ==1) z="B+" ;
+else if (x ==2) z="B" ;
+else if (x ==3)  z="C+" ;
+else if (x ==4) z="C " ;
+else if (x ==5) z= "D+ " ;
+else if (x ==6) z= "D";
+else if (x ==7) z= "F ";
+else  z="W" ;
+
+ cout << "You will get "<< z << "in this 261102. ";
+ return 0;
+
+}
